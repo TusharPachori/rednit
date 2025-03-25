caff14670aca530d882ea79a0e574f50909768a2
TusharPachori <tussharpachori@gmail.com>
2025-03-25 23:01:51 +0530

    Initial commit

---
+++ src/main/java/com/escape/plan/rednit/RednitApplication.java
@@ -0,0 +1,13 @@
+package com.escape.plan.rednit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RednitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RednitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/escape/plan/rednit/RednitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escape.plan.rednit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RednitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
